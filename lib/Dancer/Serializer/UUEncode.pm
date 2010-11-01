56d46acc487cabe62a6f7c668e682a14c1853895
Sawyer X <xsawyerx@cpan.org>
2010-11-01 10:55:16 +0200

    adding documentation

--- lib/Dancer/Serializer/UUEncode.pm
+++ lib/Dancer/Serializer/UUEncode.pm
@@ -52,3 +52,22 @@ sub to_uuencode {
 
 __END__
 
+=head1 SYNOPSIS
+
+    # in your Dancer app:
+    setting serializer => 'UUEncode';
+
+    # or in your Dancer config file:
+    serializer: 'UUEncode'
+
+=head1 DESCRIPTION
+
+This serializer serializes your data structure to UU Encoding. Since UU Encoding
+is just encoding and not a serialization format, it first freezes it using
+L<Storable> and only then serializes it.
+
+It uses L<Storable>'s C<nfreeze> function.
+
+=head1 SEE ALSO
+
+The Dancer Advent Calendar 2010.
